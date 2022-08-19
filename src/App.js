341a594afadf94595e5d672510d6ba4c719687fa
Logesh <logeshvenkatesan23@gmail.com>
2022-08-19 13:48:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import BecomeMentor from "./pages/BecomeMentor";
+import LiveWebinar from "./pages/LiveWebinar";
+
+function App(){
+  return(
+    <div>
+      <LiveWebinar />
+      <BecomeMentor />
+    </div>
+  );
+}
+
+export default App;
